6ca92d70d6ef5223797b59e6a7be2e735406d663
Tony Rizko <tony@rizkocircle.com>
2024-03-29 23:05:01 -0700

    Update header struct to be read in big-endian order

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ use binread::BinReaderExt;
 use binread::{BinRead, io::Cursor};
 
 #[derive(BinRead,Debug)]
-#[br(magic = b"MThd")]
+#[br(magic = b"MThd", big)]
 struct MidiHeader {
     format: u16,
     track_count: u16,
@@ -13,6 +13,6 @@ struct MidiHeader {
 
 fn main() {
     let mut reader = Cursor::new(File::open("output.mid").unwrap().bytes().collect::<io::Result<Vec<u8>>>().unwrap());
-    let header: MidiHeader = reader.read_ne().unwrap();
+    let header: MidiHeader = reader.read_be().unwrap();
     println!("{:?}", header);
 }
