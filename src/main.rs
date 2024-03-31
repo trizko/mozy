8620234db8e65b0807aa4552dfa9245d55170a65
Tony Rizko <tony@rizkocircle.com>
2024-03-30 23:30:50 -0700

    Add structs for midi file, tracks and events

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,53 @@ use std::io::{self, Read};
 use binread::BinReaderExt;
 use binread::{BinRead, io::Cursor};
 
+struct MidiFile {
+    pub header: MidiHeader,
+    pub tracks: Vec<MidiTrack>,
+}
+
+struct MidiTrack {
+    pub events: Vec<MidiEvent>,
+}
+
+enum MidiEvent {
+    NoteOn {
+        delta_time: u32,
+        channel: u8,
+        note_number: u8,
+        velocity: u8,
+    },
+    NoteOff {
+        delta_time: u32,
+        channel: u8,
+        note_number: u8,
+        velocity: u8,
+    },
+    ProgramChange {
+        delta_time: u32,
+        channel: u8,
+        program_number: u8,
+    },
+    ControlChange {
+        delta_time: u32,
+        channel: u8,
+        controller: u8,
+        value: u8,
+    },
+    MetaEvent {
+        delta_time: u32,
+        event_type: u8,
+        data: Vec<u8>,
+    },
+}
+
 #[derive(BinRead,Debug)]
 #[br(magic = b"MThd", big)]
 struct MidiHeader {
     length: u32,
     format: u16,
     track_count: u16,
-    delta: u16,
+    division: u16,
 }
 
 fn main() {
