10f111d3518bd6d5954ad7deda7d6bbd9e6fccbc
Tony Rizko <tony@rizkocircle.com>
2024-04-03 22:17:40 -0700

    Move parsing logic to midi_parser submodule

--- src/midi_parser.rs
+++ src/midi_parser.rs
@@ -3,7 +3,34 @@ use std::io::{self, Read};
 use binread::BinReaderExt;
 use binread::{BinRead, io::Cursor};
 
-struct MidiParser {
+#[derive(BinRead,Debug)]
+#[br(big)]
+pub struct MidiFile {
+    pub header: MidiHeader,
+
+    #[br(count = header.track_count as usize)]
+    pub tracks: Vec<MidiTrack>,
+}
+
+#[derive(BinRead,Debug)]
+#[br(magic = b"MThd", big)]
+struct MidiHeader {
+    length: u32,
+    format: u16,
+    track_count: u16,
+    division: u16,
+}
+
+#[derive(BinRead,Debug)]
+#[br(magic = b"MTrk", big)]
+struct MidiTrack {
+    length: u32,
+
+    #[br(count = length as usize)]
+    events: Vec<u8>,
+}
+
+pub struct MidiParser {
     data: Cursor<Vec<u8>>,
 }
 
@@ -13,8 +40,12 @@ impl MidiParser {
         let mut data = Vec::new();
         file.read_to_end(&mut data).expect("Failed to read file");
 
-        Self {
+        MidiParser {
             data: Cursor::new(data),
         }
+    }
+
+    pub fn parse(&mut self) -> MidiFile {
+        self.data.read_be().unwrap()
     }
 }
