df4b9950fb6ccbe57700907b89048779d7bde62a
Tony Rizko <tony@rizkocircle.com>
2024-04-14 22:06:32 -0700

    Revert to more declarative impl

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use crate::midi_parser::{MidiParser, MidiFile};
 
 fn main() {
     let file_path = "output.mid";
-    let midi_parser: MidiParser = MidiParser::new(file_path);
+    let mut midi_parser: MidiParser = MidiParser::new(file_path);
     let midi_file: MidiFile = midi_parser.parse();
     println!("{:#?}", midi_file);
 }
